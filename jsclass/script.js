4e80a3a22f5bbae06bc85d9a1ae21035416fdefd
Murad Mammadzade <mammadzade03@gmail.com>
2023-06-10 08:42:45 +0400

    added

---
+++ jsclass/script.js
@@ -0,0 +1,54 @@
+"use strict";
+//#region Quiz
+// const words=['A','B','C','D','E',
+// '-','_',')','(','*',
+// 'K','L','M','N','R',
+// 'T','W','Y','V','Z',
+// '{','}','|','=','+',
+// 'a','b','c','d','e',
+// 'f','g','h','i','j',
+// '\\',':',';','[',']',
+// 'k','l','m','n','r',
+// '&','^','%','$','#',
+// 't','w','y','v','z',
+// '6','7','8','9','0',
+// '!','@','\'','"','<',
+// 'o','p','q','r','s',
+// '1','2','3','4','5',
+// 'O','P','Q','R','S',
+// '>',',','.','?','/',
+// 'F','G','H','I','J',
+// '`','~'
+// ];
+// function Generator(){
+//     //debugger;
+//     let inp=document.getElementById('inp').value;
+//     let data='';
+//     for(let i=0;i<inp;i++){
+//          let rnd=Math.floor(Math.random()*words.length);
+//          data+=words[rnd];
+//     }
+//     alert(data);
+//  }
+ //#endregion
+//#region Class
+//  class Some{
+//     constructor(name,age,year,late,kl){
+//         this.name=name,
+//         this.age=age,
+//         this.year=year
+//         this.late=late;
+//         this.kl=kl;
+//     }
+// }
+// class Person extends Some{
+//     constructor(name,age,year,late,kl,bl){
+//         super(name,age,year,late,kl),
+//         this.bl=bl
+//     }
+// }
+// const cls=new Some(1,2,3,6,9);
+// const cs=new Person(1,2,3,4,5,2);
+// console.log(cls.kl);
+// console.log(cs.bl);
+//#endregion
